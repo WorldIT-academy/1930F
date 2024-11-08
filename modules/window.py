1f5a2008c9482e6a9ef7d0c4dfda5027669c3c40
WorldIT-academy <worldit_academy@worldit.academy>
2024-11-08 21:11:16 +0200

    Cener window

--- modules/window.py
+++ modules/window.py
@@ -2,6 +2,11 @@ import customtkinter as ctk
 
 main_window = ctk.CTk()
 main_window.title("Project")
-print("resize")
-print("Master code")
-main_window.geometry("1280x720+20+20")
+width = 1280
+height = 720
+screen_width = main_window.winfo_screenwidth()
+screen_height = main_window.winfo_screenheight()
+x = screen_width//2 - width//2
+y = screen_height//2 - height//2
+
+main_window.geometry(f"{width}x{height}+{x}+{y}")
